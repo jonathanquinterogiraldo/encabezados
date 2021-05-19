7f46391096f9bffb9767736794d1c2b89eff7b41
Jonathan Quintero Giraldo <jonathan.quintero@newshore.es>
2021-05-18 20:46:09 -0500

    fix user agent

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const app = express();   
-const browser = require('browser-detect');
 
-app.get('/', (req, res) => {
-const result = browser(req.headers['user-agent']);
-res.send(`<h1> ${result.name} </h1>`);    
+app.get('/', (req, res) => {    
+    res.send(req.headers['user-agent']);    
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
